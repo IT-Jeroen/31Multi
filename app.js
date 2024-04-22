db6d41672a651565f1c939603b4c4e6f24baa3b1
IT Jeroen <itjeroen@gmail.com>
2024-04-22 21:14:46 +1000

    Re-aranging of Button. Styling of scoreboard.

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ import {handleNameSubmitted} from './dom.js';
 // import * as dom from './dom.js';
 
 // dom.renderApp(dom.createNameInput(dom.handleNameSubmitted));
-renderApp(createNameInput(handleNameSubmitted()));
+renderApp(createNameInput(handleNameSubmitted));
